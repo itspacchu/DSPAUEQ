bb99bba3252e32d4e7c0ef851a75d48f4be2e0a1
itspacchu <37984032+itspacchu@users.noreply.github.com>
2021-09-09 22:46:22 +0530

    Some stuff

--- Fourier.h
+++ Fourier.h
@@ -3,7 +3,7 @@
 double pi = 4 * atan(1.0);
 using namespace std;
 
-bool isPowerOfTwo(int n)
+bool isPowerOfTwo(int n) // cuz fft really likes to simp powers of 2 values
 {
     return (ceil(log2(n)) == floor(log2(n))) && (n != 0);
 }
@@ -21,14 +21,6 @@ vector<complex<double>> ConvertToComplex(vector<double> RealArr)
 vector<complex<double>> FFT(vector<complex<double>> arr) // needs 2^n no of samples input!!
 {
     int n = arr.size();
-    if (isPowerOfTwo(n) == false)
-    {
-        cout << "Error: Input size is not a power of 2 Padding Zeros" << endl;
-        for (int p = 0; p < ceil(log2(n)) - n; p++)
-        {
-            arr.push_back(complex<double>(0, 0));
-        }
-    }
     if (n == 1)
     {
         return arr;
@@ -58,6 +50,38 @@ vector<complex<double>> FFT(vector<complex<double>> arr) // needs 2^n no of samp
     return freqbin;
 }
 
+vector<complex<double>> IFFT(vector<complex<double>> arr)
+{
+    int n = arr.size();
+    if (n == 1)
+    {
+        return arr;
+    }
+    // split into even and odd subsums
+    int half = n / 2;
+    vector<complex<double>> Xeven(half, 0);
+    vector<complex<double>> Xodd(half, 0);
+
+    for (int i = 0; i != half; i++)
+    {
+        Xeven[i] = arr[2 * i];
+        Xodd[i] = arr[2 * i + 1];
+    }
+    vector<complex<double>> Feven(half, 0);
+    Feven = FFT(Xeven);
+    vector<complex<double>> Fodd(half, 0);
+    Fodd = FFT(Xodd);
+    // need a single sample at end
+    vector<complex<double>> freqbin(n, 0);
+    for (int k = 0; k != half; k++)
+    {
+        complex<double> cmpexp = polar(1.0, 2 * pi * k / n) * Fodd[k] * (1 / (double)n);
+        freqbin[k] = Feven[k] + cmpexp;
+        freqbin[k + half] = Feven[k] - cmpexp;
+    }
+    return freqbin;
+}
+
 vector<complex<double>> FFT_OLD(vector<complex<double>> arr)
 {
     int n = arr.size();
@@ -73,4 +97,28 @@ vector<complex<double>> FFT_OLD(vector<complex<double>> arr)
         retarr.push_back(even[k] - t);
     }
     return retarr;
+}
+
+vector<double> CmpMagnitude(vector<complex<double>> myFFTOUTPUT)
+{
+    vector<double> myFFTOUTPUT_abs;
+    for (int i = 0; i < myFFTOUTPUT.size(); i++)
+    {
+        myFFTOUTPUT_abs.push_back(abs(myFFTOUTPUT[i]));
+    }
+    return myFFTOUTPUT_abs;
+}
+
+vector<double> DivByN(vector<double> myFFTOUTPUT_abs, int N = 0)
+{
+    if (N == 0)
+    {
+        N = myFFTOUTPUT_abs.size(); // sanity no zero division ;-;
+    }
+    vector<double> DivbyNReturn;
+    for (int i = 0; i < myFFTOUTPUT_abs.size(); i++)
+    {
+        DivbyNReturn.push_back(myFFTOUTPUT_abs[i] / (double)N);
+    }
+    return DivbyNReturn;
 }
--- testbench.cpp
+++ testbench.cpp
@@ -10,14 +10,14 @@ using namespace std;
 
 int main(void)
 {
-    vector<double> insig{1.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0};
+    vector<double> insig{12.0, 34.0, 54.0, 19.0};
     vector<complex<double>> myFFTOUTPUT = FFT(ConvertToComplex(insig));
-
+    vector<double> myIFFTOUTPUT = DivByN(CmpMagnitude(IFFT(myFFTOUTPUT)));
     ofstream myFile;
-    myFile.open("test1.txt");
-    for (int i = 0; i < myFFTOUTPUT.size(); i++)
+    myFile.open("test2.txt");
+    for (int i = 0; i < myIFFTOUTPUT.size(); i++)
     {
-        myFile << myFFTOUTPUT[i] << endl;
+        myFile << myIFFTOUTPUT[i] << endl;
     }
     myFile.close();
 }
